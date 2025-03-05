58c2e299df0dc8a13c480a8f1bc08f066c7d9758
visheraaaa <alanovg@yandex.com>
2025-03-05 14:10:47 +0300

    Добавьте файлы проекта.

---
+++ MAUIPlayer/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MAUIPlayer
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
